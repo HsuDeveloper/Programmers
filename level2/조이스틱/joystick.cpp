7c6d79c9b2fb168349adf8dcd825d24f1db43ee4
HsuDeveloper <hsu9702@naver.com>
2022-04-26 16:10:07 +0900

    solution joystick

--- level2/조이스틱/joystick.cpp
+++ level2/조이스틱/joystick.cpp
@@ -3,38 +3,48 @@
 
 using namespace std;
 
+int change_cnt(char c){
+    int a = c-'A';
+    int b = 'Z'-c+1;
+    if(a < b) return a;
+    return b; 
+}
+
 int solution(string name) {
-    int answer = 0, i = 0;
-    //A로 구성된 화면
-    string temp(name.length(), 'A');
-    while (true) {
-        //바꾸고 있는 화면에 반영하고
-        temp[i] = name[i];
-        //둘중에 적은걸로 결과에 추가하기
-        name[i] - 'A' > 'Z' + 1 - name[i] ? answer += 'Z' + 1 - name[i] : answer += name[i] - 'A';
-        //바꾼후 문자열이 동일하다면 계산 종료
-        if (temp == name)    break;
-        //왼쪽으로 갈지 오른쪽으로 갈지 계산하기 
-        for (int move = 1; move < name.length(); move++) {
-            //오른쪽 이동이 빠르다면 오른쪽으로 이동하고 이동횟수 반영
-            if (name[(i + move) % name.length()] != temp[(i + move) % name.length()]) {
-                i = (i + move) % name.length();
-                answer += move;
-                break;
-            }
-            //왼쪽으로 이동이 빠르다면 왼쪽으로 이동하고 이동횟수 반영
-            else if (name[(i + name.length() - move) % name.length()] 
-                != temp[(i + name.length() - move) % name.length()]) {
-                i = (i + name.length() - move) % name.length();
-                answer += move;
-                break;
-            }
+    int answer = 0;
+    int size = name.size();
+
+    vector<int> notA;
+    for(int i=0;i<size;i++){
+        if(name[i] != 'A'){
+            notA.push_back(i);
+            answer +=change_cnt(name[i]);
+            continue;
         }
     }
+    int notA_size = notA.size();
+    if(notA_size==0) return 0;
+
+    int left = size - notA[0];
+    if(left == size){
+        if(notA_size == 1) left = 0;
+        else left = size - notA[1];
+    }
+    
+    int tmp = min(notA[notA_size-1],left);
+
+    for(int i=0;i<notA_size-1;i++){
+        int move = min(2*notA[i] + size - notA[i+1], notA[i] + 2*size - 2*notA[i+1]);
+        tmp = min(tmp,move);
+    }
+
+    answer += tmp;
+
+    
     return answer;
 }
 
 int main(){
-    string name = "JEROAAAAAAAEN";
+    string name = "ZAAAAAA";
     cout<<solution(name);
 }
