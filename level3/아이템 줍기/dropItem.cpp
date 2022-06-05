05a1f5928bec69558998c4c522f29de594844d6d
HsuDeveloper <hsu9702@naver.com>
2022-06-05 14:37:29 +0900

    sub global

--- level3/아이템 줍기/dropItem.cpp
+++ level3/아이템 줍기/dropItem.cpp
@@ -5,11 +5,10 @@ using namespace std;
 
 int dx[4] = {1,-1,0,0};
 int dy[4] = {0,0,1,-1};
-int minD = 10001;
 
 void dfs(vector<vector<int>> &coordinate, vector<vector<bool>> d, int row, int col, int itemX,int itemY, int cnt){
     if((row==itemY) && (col==itemX)) {
-        if(minD > cnt) minD = cnt;
+        if(coordinate[row][col] > cnt) coordinate[row][col] = cnt;
         return;    
     }
 
@@ -17,7 +16,7 @@ void dfs(vector<vector<int>> &coordinate, vector<vector<bool>> d, int row, int c
         int nextRow = row + dy[i];
         int nextCol = col + dx[i];
 
-        if(nextRow<0||nextRow>100||nextCol<0||nextCol>100||d[nextRow][nextCol]||coordinate[nextRow][nextCol]==2||!coordinate[nextRow][nextCol]) continue;
+        if(nextRow<0||nextRow>100||nextCol<0||nextCol>100||d[nextRow][nextCol]||coordinate[nextRow][nextCol]==-1||!coordinate[nextRow][nextCol]) continue;
         d[nextRow][nextCol] = 1;
         dfs(coordinate,d,nextRow,nextCol,itemX,itemY,cnt+1);
         d[nextRow][nextCol] = 0;
@@ -28,17 +27,17 @@ void dfs(vector<vector<int>> &coordinate, vector<vector<bool>> d, int row, int c
 
 void draw(vector<int> &rec, vector<vector<int>> &coordinate){
     for(int i= rec[0]; i<=rec[2];i++){
-        if(coordinate[rec[1]][i] != 2) coordinate[rec[1]][i] = 1;
-        if(coordinate[rec[3]][i] != 2) coordinate[rec[3]][i] = 1;
+        if(coordinate[rec[1]][i] != -1) coordinate[rec[1]][i] = 1;
+        if(coordinate[rec[3]][i] != -1) coordinate[rec[3]][i] = 1;
     }
     for(int i = rec[1]; i<=rec[3]; i++){
-        if(coordinate[i][rec[0]] != 2) coordinate[i][rec[0]] = 1;
-        if(coordinate[i][rec[2]] != 2) coordinate[i][rec[2]] = 1;
+        if(coordinate[i][rec[0]] != -1) coordinate[i][rec[0]] = 1;
+        if(coordinate[i][rec[2]] != -1) coordinate[i][rec[2]] = 1;
     }
 
     for(int i = rec[0]+1; i<rec[2]; i++){
         for(int j = rec[1]+1; j<rec[3]; j++){
-            coordinate[j][i] = 2;
+            coordinate[j][i] = -1;
         }
     }
 
@@ -62,9 +61,10 @@ int solution(vector<vector<int>> rectangle, int characterX, int characterY, int 
         draw(rectangle[i],coordinate);
     }
 
+    coordinate[itemY][itemX] = 10001;
     dfs(coordinate,d,characterY,characterX,itemX,itemY,0);
 
-    answer = minD/2;
+    answer = coordinate[itemY][itemX]/2;
 
     return answer;
 }
